c9fec566f29907e5f00b5c490b2f8a41596e3af5
rio.ishii <study.it.git@gmail.com>
2021-04-26 05:55:01 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,10 +24,10 @@
                 </div>
             </nav>
         </header>
-        @include('commons.navbar')
+       
         
         <div class="container">
-            @include('commons.error_tasks')
+           
             
             @yield('content')
         </div>
