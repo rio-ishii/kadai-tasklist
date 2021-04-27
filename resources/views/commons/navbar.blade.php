5333e9b8e3a1bed7949262c9cd454f1b08fad412
rio.ishii <study.it.git@gmail.com>
2021-04-27 07:36:18 +0000

    user registration

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,6 @@
+ <ul class="nav navbar-nav navbar-right">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li><a href="#">Login</a></li>
+                </ul>
